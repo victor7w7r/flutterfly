ee99645961ed0b8045cbabb2ff6a970c52c9c9aa
Victor Jiménez <vcjimenez1@espe.edu.ec>
2022-07-03 07:41:58 -0500

    UpdateCodeDistribution

---
+++ lib/flutterfly.dart
@@ -0,0 +1,90 @@
+import 'dart:io' show Platform;
+
+import 'package:flutter/cupertino.dart' show CupertinoApp;
+import 'package:flutter/material.dart' show MaterialApp;
+import 'package:flutter/widgets.dart';
+
+import 'package:provider/provider.dart' show Provider;
+import 'package:fluent_ui/fluent_ui.dart' show FluentApp;
+
+import 'package:flutterfly/desktop_selector.dart';
+
+import 'package:flutterfly/modules/material/providers/theme_provider.dart';
+import 'package:flutterfly/modules/cupertino/providers/theme_provider.dart';
+import 'package:flutterfly/modules/fluent/providers/theme_provider.dart';
+
+import 'package:flutterfly/routers/cupertino_router.gr.dart';
+import 'package:flutterfly/routers/material_router.gr.dart';
+import 'package:flutterfly/routers/fluent_router.gr.dart';
+
+final _materialAppRouter = MaterialAppRouter();
+final _cupertinoAppRouter = CupertinoAppRouter();
+final _fluentAppRouter = FluentAppRouter();
+
+class FlutterflyApp extends StatefulWidget {
+  const FlutterflyApp({Key? key}) : super(key: key);
+
+  @override
+  State<FlutterflyApp> createState() => _FlutterflyAppState();
+}
+
+class _FlutterflyAppState extends State<FlutterflyApp> {
+
+  bool _isDesktop = false;
+  String _desktopSelector = '';
+
+  @override
+  Widget build(BuildContext context) {
+    if(Platform.isIOS && !_isDesktop) {
+      return _cupertinoApp(context, _cupertinoAppRouter);
+    } else if (Platform.isAndroid && !_isDesktop) {
+      return _materialApp(context, _materialAppRouter);
+    } else if (_desktopSelector != '' && _isDesktop) {
+      switch(_desktopSelector) {
+        case 'material':
+          return _materialApp(context, _materialAppRouter);
+        case 'cupertino':
+          return _cupertinoApp(context, _cupertinoAppRouter);
+        default:
+          return _fluentApp(context, _fluentAppRouter);
+      }
+    } else {
+      return DesktopSelector(callback: (value) {
+        setState(() {
+          _desktopSelector = value;
+          _isDesktop = true;
+        });
+      });
+    }
+  }
+
+  CupertinoApp _cupertinoApp(BuildContext context, CupertinoAppRouter desktopAppRouter) {
+    return CupertinoApp.router(
+      title: 'flutterfly',
+      debugShowCheckedModeBanner: false,
+      routerDelegate: desktopAppRouter.delegate(),
+      routeInformationParser: desktopAppRouter.defaultRouteParser(),
+      theme: Provider.of<ThemeCupertinoProvider>(context).currentTheme
+    );
+  }
+
+  MaterialApp _materialApp(BuildContext context, MaterialAppRouter materialAppRouter) {
+    return MaterialApp.router(
+      title: 'flutterfly',
+      debugShowCheckedModeBanner: false,
+      routerDelegate: materialAppRouter.delegate(),
+      routeInformationParser: materialAppRouter.defaultRouteParser(),
+      theme: Provider.of<ThemeMaterialProvider>(context).currentTheme
+    );
+  }
+
+  FluentApp _fluentApp(BuildContext context, FluentAppRouter desktopAppRouter) {
+    return FluentApp.router(
+      title: 'flutterfly',
+      debugShowCheckedModeBanner: false,
+      routerDelegate: desktopAppRouter.delegate(),
+      routeInformationParser: desktopAppRouter.defaultRouteParser(),
+      color: Provider.of<ThemeFluentProvider>(context).color,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,50 @@
 import 'dart:io' show Platform;
 
-import 'package:flutter/widgets.dart';
+import 'package:flutter/widgets.dart' show WidgetsFlutterBinding, runApp, Color;
+import 'package:provider/provider.dart' show MultiProvider, ChangeNotifierProvider;
+import 'package:provider/single_child_widget.dart';
 
-import 'package:provider/provider.dart' show MultiProvider;
+import 'package:flutterfly/flutterfly.dart';
 
-import 'package:flutterfly/providers/dynamic_providers.dart';
 import 'package:flutterfly/share_preferences/preferences.dart';
-import 'package:flutterfly/desktop_selector.dart';
-import 'package:flutterfly/platforms.dart';
+import 'package:flutterfly/providers/data_provider.dart';
+import 'package:flutterfly/providers/binance_provider.dart';
 
-import 'package:flutterfly/routers/cupertino_router.gr.dart';
-import 'package:flutterfly/routers/material_router.gr.dart';
-import 'package:flutterfly/routers/fluent_router.gr.dart';
+import 'package:flutterfly/modules/material/providers/theme_provider.dart';
+import 'package:flutterfly/modules/cupertino/providers/theme_provider.dart';
+import 'package:flutterfly/modules/fluent/providers/theme_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Preferences.init();
-  runApp(MultiProvider(providers: dynamicProviders(), child: const App()));
+  runApp(MultiProvider(providers: dynamicProviders(), child: const FlutterflyApp()));
 }
 
-final _materialAppRouter = MaterialAppRouter();
-final _cupertinoAppRouter = CupertinoAppRouter();
-final _fluentAppRouter = FluentAppRouter();
-
-class App extends StatefulWidget {
-  const App({Key? key}) : super(key: key);
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-
-  bool _isDesktop = false;
-  String _desktopSelector = '';
-
-  @override
-  Widget build(BuildContext context) {
-    if(Platform.isIOS && !_isDesktop) {
-      return cupertinoApp(context, _cupertinoAppRouter);
-    } else if (Platform.isAndroid && !_isDesktop) {
-      return materialApp(context, _materialAppRouter);
-    } else if (_desktopSelector != '' && _isDesktop) {
-      switch(_desktopSelector) {
-        case 'material':
-          return materialApp(context, _materialAppRouter);
-        case 'cupertino':
-          return cupertinoApp(context, _cupertinoAppRouter);
-        default:
-          return fluentApp(context, _fluentAppRouter);
-      }
-    } else {
-      return DesktopSelector(callback: (value) {
-        setState(() {
-          _desktopSelector = value;
-          _isDesktop = true;
-        });
-      });
-    }
+List<SingleChildWidget> dynamicProviders() {
+  if(Platform.isIOS) {
+    return [
+        ChangeNotifierProvider(create: ( _ ) =>
+          ThemeCupertinoProvider(isDarkmode: Preferences.isDarkmode, darkState: Preferences.isDarkmode)),
+        ChangeNotifierProvider(create: ( _ ) => DataProvider(data: "")),
+        ChangeNotifierProvider(create: ( _ ) => BinanceProvider(), lazy: false)
+    ];
+  } else if(Platform.isAndroid) {
+    return [
+        ChangeNotifierProvider(create: ( _ ) =>
+          ThemeMaterialProvider(isDarkmode: Preferences.isDarkmode, darkState: Preferences.isDarkmode)),
+        ChangeNotifierProvider(create: ( _ ) => DataProvider(data: "")),
+        ChangeNotifierProvider(create: ( _ ) => BinanceProvider(), lazy: false)
+    ];
+  } else {
+    return [
+      ChangeNotifierProvider(create: ( _ ) =>
+        ThemeFluentProvider(darkState: false, color: const Color.fromRGBO(255, 255, 255, 0))),
+      ChangeNotifierProvider(create: ( _ ) =>
+        ThemeCupertinoProvider(isDarkmode: Preferences.isDarkmode, darkState: Preferences.isDarkmode)),
+      ChangeNotifierProvider(create: ( _ ) =>
+        ThemeMaterialProvider(isDarkmode: Preferences.isDarkmode, darkState: Preferences.isDarkmode)),
+      ChangeNotifierProvider(create: ( _ ) => DataProvider(data: "")),
+      ChangeNotifierProvider(create: ( _ ) => BinanceProvider(), lazy: false)
+    ];
   }
 }
