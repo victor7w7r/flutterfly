7a2b3197702ed30b7ed341f3c1dfc4795a4721d2
victor7w7r <43785784+victor7w7r@users.noreply.github.com>
2023-10-19 19:50:18 +0000

    Deploying to gh-pages from @ victor7w7r/flutterfly@c1cded4a659da725de1d4ffe79bae2a41aaf13dc 🚀

--- main.dart.js
+++ main.dart.js
@@ -27899,7 +27899,7 @@ $1(a){var s=$.cB
 s=(s==null?$.cB=A.fF(self.window.flutterConfiguration):s).b
 if(s==null)s=null
 else{s=s.canvasKitBaseUrl
-if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/a794cf2681c6c9fe7b260e0e84de96298dc9c18b/":s)+a},
+if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/767d8c75e898091b925519803830fc2721658d07/":s)+a},
 $S:66}
 A.ay2.prototype={
 $1(a){this.a.remove()
