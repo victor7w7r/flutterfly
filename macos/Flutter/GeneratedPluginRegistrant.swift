c3dc8f386e202d4c4794c7f116d398e6ff67c29e
victor7w7r <43785784+victor7w7r@users.noreply.github.com>
2024-07-14 08:31:23 -0500

    UpdatePackages

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import bitsdojo_window_macos
 import path_provider_foundation
+import patrol
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BitsdojoWindowPlugin.register(with: registry.registrar(forPlugin: "BitsdojoWindowPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PatrolPlugin.register(with: registry.registrar(forPlugin: "PatrolPlugin"))
 }
